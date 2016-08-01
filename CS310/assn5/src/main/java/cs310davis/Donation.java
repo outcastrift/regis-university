c525acd255c6cc6d850c1dc14596db3d65824604
samuel.davis <sam.davis@techngs.com>
2016-07-31 21:15:30 -0400

    Finished Donor conversion to Hashed map

--- CS310/assn5/src/main/java/cs310davis/Donation.java
+++ CS310/assn5/src/main/java/cs310davis/Donation.java
@@ -273,21 +273,7 @@ public class Donation {
 
         return isEqual;
     }
-
-
-    @Override
-    public int hashCode(){
-        int hashCode =0;
-        String idVar = String.valueOf(donationId);
-        char[] charArray = idVar.toCharArray();
-
-
-        for(char c : charArray){
-            hashCode= hashCode + (int) c;
-        }
-        hashCode = hashCode % 23;
-        return hashCode;
-    }
+    
     /**
      * To string string.
      *
